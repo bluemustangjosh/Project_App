ddeac1fcdfc71ed04ce1cc55e9ce1927d8e0799f
ElRaudelFG <raudelfg01@gmail.com>
2026-08-04 18:01:42 -0500

    Removed testApp for organization.

--- project/classes/models.py
+++ project/classes/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models    
 from django.contrib.auth.models import User
+# Create your models here.
 
 class Class(models.Model):
     student = models.ForeignKey(User, on_delete=models.CASCADE)
