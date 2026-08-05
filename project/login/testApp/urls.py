e0d527d560a28ce0d078076fdc8b6872c7bf3af0
Debs305 <66663079+Debs305@users.noreply.github.com>
2026-08-05 12:08:58 -0500

    Delete project/login directory

--- project/login/testApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import views as auth_views
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
-]
