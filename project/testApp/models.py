6ac64da6ebd2c67d9df19eeea6f3fadb26b0f99d
bluemustangjosh <joshuamedrano@gmail.com>
2026-08-11 11:08:33 -0500

    connected the add class page after logging in only if the student doesnt have a class. connected the class database to the add class page. still need to test it out.

--- project/testApp/models.py
+++ project/testApp/models.py
@@ -20,6 +20,7 @@ class Student(models.Model):
     student_id = models.CharField(max_length=20, unique=True)
     username = models.CharField(max_length=30, unique=True)
     password = models.CharField(max_length=128)
+    classes = models.ManyToManyField(Class, blank=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.student_id})"
