7df7d12f16b4262b3f17be58a818244dd95bfd90
Debs305 <joeoffice2012@gmail.com>
2026-08-19 15:45:14 -0500

    Added DS Store to gitignore file itself in VS code

--- project/classes/admin.py
+++ project/classes/admin.py
@@ -1,8 +1,3 @@
-from django.contrib import admin
-from .models import Class
-
-admin.site.register(Class)
-
 # Register your models here.
 from django.contrib import admin
 from .models import Class
